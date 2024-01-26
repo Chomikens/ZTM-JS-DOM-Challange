a1a105537a786e7efe57bdaa64f7c1a84ce94de0
Dominik <kotorozec.code@gmail.com>
2024-01-26 15:41:51 +0100

    Get necessary elements

---
+++ app.js
@@ -0,0 +1,7 @@
+// Get necessary elements
+
+const shoppingListWrapper = document.querySelector('.shoppingListWrapper'); // Selects the main wrapper element for the shopping list.
+const itemListUl = shoppingListWrapper.querySelector('.jsItemList'); // Selects the unordered list element where the items will be displayed.
+const itemInput = shoppingListWrapper.querySelector('#item-add'); // Selects the input element where new items are entered.
+const listErrorText = shoppingListWrapper.querySelector('.jsListStatus') // Selects the element where error messages related to the list will be displayed.
+
