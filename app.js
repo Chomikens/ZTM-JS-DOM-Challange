f882bd8c6f758568e128b2440a8aedafae8ba70e
Dominik <kotorozec.code@gmail.com>
2024-01-26 15:42:56 +0100

    Input values check and get

--- app.js
+++ app.js
@@ -5,3 +5,29 @@ const itemListUl = shoppingListWrapper.querySelector('.jsItemList'); // Selects 
 const itemInput = shoppingListWrapper.querySelector('#item-add'); // Selects the input element where new items are entered.
 const listErrorText = shoppingListWrapper.querySelector('.jsListStatus') // Selects the element where error messages related to the list will be displayed.
 
+/**
+ * Checks if the input element has a non-empty value.
+ * @param {HTMLInputElement} itemInput - The input element to validate.
+ * @returns {boolean} - Returns true if input has a value, false otherwise.
+ */
+
+function checkInputValue(itemInput) {
+    if (!itemInput.value) { 
+        listErrorText.textContent = "Please add an item!";
+        return false;
+    } else {
+        listErrorText.textContent = "";
+        return true;
+    }
+}
+
+
+/**
+ * Retrieves the value of the input field.
+ * @returns {String} - The value of the input field.
+ */
+
+function getValue() {
+    return itemInput.value;
+}
+
